08cc7d75dc320c41bc133e1e1b4627b9b5ca0a50
yasmin-emad <yasmin.emad600@gmail.com>
2023-03-13 14:24:41 +0200

    login and get users data

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,69 +1,12 @@
-// import { Component } from '@angular/core';
-// import { AngularFireAuth } from '@angular/fire/compat/auth';
-// // import { AngularFirestore } from '@angular/fire/compat/firestore';
-
-// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-// import { Router } from '@angular/router';
-// // import { ToastrService } from 'ngx-toastr';
-// // import { FirebaseCodeErrorService } from 'src/app/services/firebase-code-error.service';
-
-// @Component({
-//   selector: 'app-login',
-//   templateUrl: './login.component.html',
-//   styleUrls: ['./login.component.scss']
-// })
-// export class LoginComponent {
-
-//   loginUser: FormGroup;
-//   loading: boolean = false;
-
-//   constructor(
-//     private fb: FormBuilder,
-//     private afAuth: AngularFireAuth,
-//     // private toastr: ToastrService,
-//     private router: Router,
-//     // private firebaseError: FirebaseCodeErrorService
-//   ) {
-//     this.loginUser = this.fb.group({
-//       email: ['', [Validators.required, Validators.email]],
-//       password: ['', Validators.required],
-//     })
-//   }
-
-//   ngOnInit(): void {}
-
-//   login() {
-//     const email = this.loginUser.value.email;
-//     const password = this.loginUser.value.password;
-
-//     this.loading = true;
-//     this.afAuth.signInWithEmailAndPassword(email, password).then((user) => {
-//       this.router.navigate(['/engineer']);
-//       // if(user.user?.emailVerified) {
-//       //   this.router.navigate(['/dashboard']);
-//       // } else {
-//       //   this.router.navigate(['/engineer']);
-//       // }
-//     }).catch((error) => {
-//       this.loading = false;
-//       console.log('Error ' + error.code);
-//       // this.toastr.error(this.firebaseError.codeError(error.code), );
-//     })
-//   }
-
-// }
-
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthServiceService } from '../services/auth-service.service'
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-// import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-    // styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
@@ -85,29 +28,29 @@ export class LoginComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        if (this.authService.userLoggedIn) {  
+        if (this.authService.userLoggedIn) {
             this.router.navigate(['/engineer']);
         }
     }
 
     loginUser() {
-        this.isProgressVisible = true;                          
+        this.isProgressVisible = true;
 
         if (this.loginForm.invalid)
             return;
 
         this.authService.loginUser(this.loginForm.value.email, this.loginForm.value.password).then((result) => {
-            this.isProgressVisible = false; 
-            // console.log(result);                    
-            if (result == null) {   
-              if (this.authService.userLoggedIn) {                            
+            this.isProgressVisible = false;
+            // console.log(result);
+            if (result == null) {
+              if (this.authService.userLoggedIn) {
                 console.log('logging in...');
-                this.router.navigate(['/engineer']); 
+                this.router.navigate(['/engineer']);
               }
               else{
                 console.log("you are not admin");
                 // this.loginForm.reset();
-              }             
+              }
             }
             else if (result.isValid == false) {
                 console.log('login error', result);
